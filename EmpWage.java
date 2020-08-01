f1b15a89362200b927b7c477f4a563cfd971b643
Aakash Malvankar <10ajax649hamilton@gmail.com>
2020-08-01 16:06:47 +0530

    Switch Case

--- EmpWage.java
+++ EmpWage.java
@@ -12,21 +12,23 @@ class Attendence{
 	{
 	int Emp_Wage_per_Hr=20;
 	int full_day=8,part_day=4,EmpWage,hrs;
-	double p=Math.floor(Math.random()*10)%3;
-	if(p == 1) 
+	int p=(int)Math.floor(Math.random()*10)%3;
+	switch(p) 
 	{
-		System.out.println("Present");
-		hrs=full_day;
-	}
-	else if(p == 2)
-	{
- 		System.out.println("Part Day");
-                hrs=part_day;
-	}
-	else
-	{
-		System.out.println("Absent");
-		hrs=0;
+	case 1:
+	System.out.println("Present");
+	hrs=full_day;
+	break;
+	
+	case 2:
+ 	System.out.println("Part Day");
+        hrs=part_day;
+	break;
+
+	default:
+	System.out.println("Absent");
+	hrs=0;
+	break;
 	}
 	EmpWage=Emp_Wage_per_Hr*hrs;
 	System.out.print("Employee Wage: "+EmpWage);
