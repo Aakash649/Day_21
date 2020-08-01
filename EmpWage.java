6905a139bb00a1f97c19e4b69ebd7795f03dc077
Aakash Malvankar <10ajax649hamilton@gmail.com>
2020-08-01 16:46:01 +0530

    20 Day Salary

--- EmpWage.java
+++ EmpWage.java
@@ -11,27 +11,32 @@ class Attendence{
 	public void attend() 
 	{
 	int Emp_Wage_per_Hr=20;
-	int full_day=8,part_day=4,EmpWage,hrs;
-	int p=(int)Math.floor(Math.random()*10)%3;
-	switch(p) 
-	{
-	case 1:
-	System.out.println("Present");
-	hrs=full_day;
-	break;
+	int full_day=8,part_day=4,EmpWage,hrs,Total_Wage=0;
 	
-	case 2:
- 	System.out.println("Part Day");
-        hrs=part_day;
-	break;
-
-	default:
-	System.out.println("Absent");
-	hrs=0;
-	break;
+	for(int i=1;i<=20;i++)
+	{
+		int p=(int)Math.floor(Math.random()*10)%3;
+		switch(p) 
+		{
+		case 1:
+		System.out.println("Present");
+		hrs=full_day;
+		break;
+		case 2:
+		System.out.println("Part Day");
+		hrs=part_day;
+		break;
+		default:
+		System.out.println("Absent");
+		hrs=0;
+		break;
+		}
+		EmpWage=Emp_Wage_per_Hr*hrs;
+		System.out.println("Employee Wage: "+EmpWage+" On Day: " +i);
+		Total_Wage=Total_Wage+EmpWage;
+		
 	}
-	EmpWage=Emp_Wage_per_Hr*hrs;
-	System.out.print("Employee Wage: "+EmpWage);
+	System.out.println("Total Wage: "+Total_Wage);
 	}
 	
 }
