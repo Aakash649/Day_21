ec1aa77a0dbb9a5f978d4a493b4ee56d8aa1ced6
Aakash Malvankar <10ajax649hamilton@gmail.com>
2020-08-01 15:22:14 +0530

    Attendence

--- EmpWage.java
+++ EmpWage.java
@@ -5,7 +5,24 @@ class Hello
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 	}
 }
-
+class Attendence{
+	
+	
+	public void attend() 
+	{
+	double p=Math.floor(Math.random()*10)%2;
+	if(p == 1) 
+	{
+		System.out.println("Present");
+	}
+	else
+	{
+		System.out.println("Absent");
+	}
+	
+	}
+	
+}
 
 class EmpWage
 {
@@ -13,5 +30,7 @@ class EmpWage
 	{
 		Hello A1 = new Hello();
 		A1.Welcome();
+		Attendence A=new Attendence();
+		A.attend();
 	}
 }
