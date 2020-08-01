2196cec3c5f615d0a73bd88c4f3c4fb96e69322e
Aakash Malvankar <10ajax649hamilton@gmail.com>
2020-08-01 17:56:18 +0530

    While Loop

--- EmpWage.java
+++ EmpWage.java
@@ -10,28 +10,39 @@ class Attendence{
 	
 	public void attend() 
 	{
-	int Emp_Wage_per_Hr=20;
-	int full_day=8,part_day=4,EmpWage,hrs;
-	int p=(int)Math.floor(Math.random()*10)%3;
-	switch(p) 
-	{
-	case 1:
-	System.out.println("Present");
-	hrs=full_day;
-	break;
+	int Emp_Wage_per_Hr=20,day=0,Total_Hrs=0;
+	int full_day=8,part_day=4,EmpWage,hrs,Total_Wage=0;
 	
-	case 2:
- 	System.out.println("Part Day");
-        hrs=part_day;
-	break;
-
-	default:
-	System.out.println("Absent");
-	hrs=0;
-	break;
+	while(Total_Hrs < 200 && day < 20)
+	{
+		int p=(int)Math.floor(Math.random()*10)%3;
+		switch(p) 
+		{
+		case 1:
+		System.out.println("Present");
+		hrs=full_day;
+		day=day+1;
+		Total_Hrs=Total_Hrs+hrs;
+		break;
+		case 2:
+		System.out.println("Part Day");
+		hrs=part_day;
+		day=day+(1/2);
+		Total_Hrs=Total_Hrs+hrs;
+		break;
+		default:
+		System.out.println("Absent");
+		hrs=0;
+		day=day+0;
+		Total_Hrs=Total_Hrs+hrs;
+		break;
+		}
+		EmpWage=Emp_Wage_per_Hr*hrs;
+		System.out.println("Employee Wage: "+EmpWage+" Total Hrs: "+Total_Hrs+" On Day: " +day);
+		Total_Wage=Total_Wage+EmpWage;
+		
 	}
-	EmpWage=Emp_Wage_per_Hr*hrs;
-	System.out.print("Employee Wage: "+EmpWage);
+	System.out.println("Total Wage: "+Total_Wage);
 	}
 	
 }
